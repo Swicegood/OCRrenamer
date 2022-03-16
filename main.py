af3be617824718c32ce7e741bd61d94a9aaa8760
Jagadguru <jguru108@gmail.com>
2022-03-15 22:27:29 -0400

    add suggested filenams

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import os
 import glob
 import platform
 import time
+import io
+import subprocess
 from datetime import datetime
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QProcess
 from PyQt5.QtWidgets import (QApplication, QDialog, QPushButton, QLabel, QLineEdit, 
         QGridLayout, QFileDialog, QListView, QTreeView, QFileSystemModel,
-        QAbstractItemView, QListWidget, QTextEdit, QCheckBox, QFrame, QMessageBox)
+        QAbstractItemView, QListWidget, QTextEdit, QCheckBox, QFrame, QMessageBox,
+        QButtonGroup)
 
 global selected_files
 global docindex
@@ -34,6 +37,13 @@ class MainWindow(QDialog):
         self.suggested1 = QLineEdit()
         self.suggested2 = QLineEdit()
         self.suggested3 = QLineEdit()
+        self.sug1Check = QCheckBox()
+        self.sug2Check = QCheckBox()
+        self.sug3Check = QCheckBox()
+        self.buttongroup = QButtonGroup(self)
+        self.buttongroup.addButton(self.sug1Check)
+        self.buttongroup.addButton(self.sug2Check)
+        self.buttongroup.addButton(self.sug3Check)
         self.saveas = QLineEdit()
         self.saveas.setMinimumWidth(500)
         self.infoLabel.setFrameStyle(QFrame.Panel | QFrame.Raised)
@@ -48,8 +58,11 @@ class MainWindow(QDialog):
         layout.addWidget(self.dateCheckBox, 2, 6, 1, 1)
         layout.addWidget(self.suggestedLabel, 3, 3, 1, 1, Qt.AlignBottom)
         layout.addWidget(self.suggested1, 4, 3, 1, 4)
+        layout.addWidget(self.sug1Check, 4, 7, 1, 1)
         layout.addWidget(self.suggested2, 5, 3, 1, 4)
+        layout.addWidget(self.sug2Check, 5, 7, 1, 1)
         layout.addWidget(self.suggested3, 6, 3, 1, 4)
+        layout.addWidget(self.sug3Check, 6, 7, 1, 1)
         layout.addWidget(self.saveasLabel, 7, 3, 1, 1, Qt.AlignBottom)
         layout.addWidget(self.saveas, 8, 3, 1, 4)
         layout.addWidget(self.backButton, 9, 1)
@@ -59,7 +72,27 @@ class MainWindow(QDialog):
         self.setLayout(layout)        
         self.makeButton.clicked.connect(self.onMakeSearchableClicked)
         self.backButton.clicked.connect(self.onBackButton)
-        self.nextButton.clicked.connect(self.onNextButton)       
+        self.nextButton.clicked.connect(self.onNextButton)
+        self.buttongroup.buttonClicked.connect(self.updatefromsuggested)
+        # self.sug1Check.stateChanged.connect(self.updatefromsuggested)
+        # self.sug2Check.stateChanged.connect(self.updatefromsuggested)
+        # self.sug3Check.stateChanged.connect(self.updatefromsuggested)
+        self.dateCheckBox.stateChanged.connect(self.state_changed)
+        self.buttongroup.setExclusive(True)
+        # QProcess object for external app
+        self.ocrproc = QProcess(self)
+        # QProcess emits `readyRead` when there is data to be read
+        self.ocrproc.readyRead.connect(self.dataReady)     
+    
+        self.ocrproc.started.connect(lambda: self.makeButton.setEnabled(False))
+        self.ocrproc.finished.connect(self.onOcrFinished)
+
+
+    def dataReady(self):
+        cursor = self.wordBox.textCursor()
+        cursor.movePosition(cursor.End)
+        cursor.insertText(str(self.ocrproc.readAll()))
+        self.wordBox.ensureCursorVisible()
 
     def fillInfoBox(self):
         path = selected_files[docindex]
@@ -73,29 +106,34 @@ class MainWindow(QDialog):
     def onMakeSearchableClicked(self):
         global selected_files
         global docindex
-        viewable_file = selected_files[docindex]        
-        mtime_seconds = os.path.getmtime(viewable_file)
-        time_seconds = time.time()
+        viewable_file = selected_files[docindex] 
         if len(viewable_file):
 
             ext = os.path.splitext(viewable_file)[1]
 
             if ext == '.pdf' or ext == '.PDF':
-                success = os.system('pdf2pdfocr/pdf2pdfocr.py -t -i "'+viewable_file+'"')
-                filebase = viewable_file.split('.')[0]
-                if filebase.split('-')[-1] != 'OCR':
-                    selected_files[docindex] = filebase+'-OCR.pdf'
-                if self.removeOrginalCheckBox.isChecked() and success == 0:
-                    os.remove(viewable_file)                    
-                viewable_file = selected_files[docindex]
-                os.utime(viewable_file, (time_seconds, mtime_seconds))
-                self.removeOrginalCheckBox.setChecked(False)
-            self.load_viewable_file(viewable_file)           
+                self.ocrproc.start('pdf2pdfocr/pdf2pdfocr.py', ['-t', '-i', viewable_file])
+  
+    def onOcrFinished(self):
+        self.makeButton.setEnabled(True)
+        viewable_file = selected_files[docindex]        
+        mtime_seconds = os.path.getmtime(viewable_file)
+        time_seconds = time.time()
+        filebase = viewable_file.split('.')[0]
+        if filebase.split('-')[-1] != 'OCR':
+            selected_files[docindex] = filebase+'-OCR.pdf'
+        if self.removeOrginalCheckBox.isChecked() and self.ocrproc.exitCode() == 0:
+            os.remove(viewable_file)                    
+        viewable_file = selected_files[docindex]
+        os.utime(viewable_file, (time_seconds, mtime_seconds))
+        self.removeOrginalCheckBox.setChecked(False)
+        self.load_viewable_file(viewable_file)           
             
     def load_wordbox(self):
         page = self.v.currentPage()
         full_text = page.text(page.rect())
         self.wordBox.setPlainText(full_text)
+        return full_text
 
     def load_viewable_file(self, viewable_file):
         if viewable_file:
@@ -116,9 +154,58 @@ class MainWindow(QDialog):
             self.setWindowTitle(viewable_file)
         self.v.setViewMode(qpageview.FitBoth)        # shows the full page
 
-        self.load_wordbox()
-        self.fillInfoBox() 
+        full_text = self.load_wordbox()
+        self.fillInfoBox()
+        self.saveas.setText(os.path.basename(viewable_file)) 
+        self.suggested1.setText(head_in_snake_case(full_text, 14))
+        base = os.path.basename(viewable_file).split('.')[0:-1]
+        base = '.'.join(base)
+        self.suggested3.setText(base)
         self.v.show()
+
+    def state_changed(self, int):
+        if self.dateCheckBox.isChecked():
+            self.onAddDateToFnameChecked()
+        else:
+            self.onAddDateToFnameUnchecked()
+
+    def onAddDateToFnameChecked(self):
+        viewable_file = selected_files[docindex]        
+        mtime_seconds = os.path.getmtime(viewable_file)
+        mtime_pretty = datetime.utcfromtimestamp(mtime_seconds).strftime('%Y-%m-%dT%H%M%S')
+        saveasfname = self.saveas.text()
+        ext = saveasfname.split('.')[-1]
+        base = saveasfname.split('.')[0:-1]
+        base = '.'.join(base)
+        self.saveas.setText(base +'_'+ mtime_pretty +'.'+ ext)
+
+    def onAddDateToFnameUnchecked(self):
+        saveasfname = self.saveas.text()
+        ext = saveasfname.split('.')[-1]
+        oldbase = saveasfname.split('.')[0:-1]
+        oldbase = '.'.join(oldbase)
+        newbase = oldbase.split('_')[0:-1]
+        newbase = '_'.join(newbase)
+        self.saveas.setText(newbase + '.' + ext)
+    
+    def updatefromsuggested(self):
+        date = ''
+        newbase = ''
+        button = self.buttongroup.checkedButton()
+        if button == self.sug1Check:
+            newbase = self.suggested1.text()
+        if button == self.sug2Check:
+            newbase = self.suggested2.text()
+        if button == self.sug3Check:
+            newbase = self.suggested3.text()        
+        saveasfname = self.saveas.text()
+        oldbase = saveasfname.split('.')[0:-1]
+        oldbase = '.'.join(oldbase)
+        if self.dateCheckBox.isChecked():
+            date = '_' + oldbase.split('_')[-1]
+        ext = saveasfname.split('.')[-1]
+        self.saveas.setText(newbase + date + '.' + ext)
+
 
     def onBackButton(self):
         global docindex
@@ -130,7 +217,7 @@ class MainWindow(QDialog):
     def onNextButton(self):
         global docindex
         global selected_files
-        if docindex < len(selected_files):
+        if docindex < len(selected_files) - 1:
             docindex += 1
         self.load_viewable_file(selected_files[docindex])
 
@@ -210,6 +297,21 @@ def creation_date(path_to_file):
                 # We're probably on Linux. No easy way to get creation dates here,
                 # so we'll settle for when its content was last modified.
                 return stat.st_mtime
+
+def head_in_snake_case(text, numofwords):
+    words = text.split(' ')
+    head = ""
+    i = 0
+    j = 0 
+    while(j < numofwords and i < len(words)):
+        if '\t' not in words[i] and '\n' not in words[i] and words[i] != '':
+            if j < numofwords - 1:
+                head += words[i]+'_'
+            else:
+                head += words[i]
+            j += 1
+        i += 1
+    return head
 
 if __name__ == '__main__':
 
